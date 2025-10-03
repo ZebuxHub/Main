40f764a169bb0fc3992c3e7018f279e91e0f2b6d
ZebuxHub <suworawut.s@gmail.com>
2025-10-03 17:42:35 +0700

    Update AutoFishSystem.lua

--- AutoFishSystem.lua
+++ AutoFishSystem.lua
@@ -359,7 +359,6 @@ function AutoFishSystem.Init(dependencies)
 	
 	frostSpotToggle = FishTab:Toggle({
 		Title = "🧊 Cast at Frost Spot",
-		Desc = "Automatically cast at Frost Spot (FX_Fish_Special) when it appears",
 		Value = FishingConfig.FrostSpotEnabled,
 		Callback = function(state)
 			print("AutoFish Frost Spot callback triggered:", state)
